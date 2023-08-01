c570c0441d9fda7d6cbe851bc71910e83ba6c580
jemcv <rangothechameleon@gmail.com>
2023-08-01 23:39:38 +0800

    Update responsive header

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,26 +5,29 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Portfolio() {
   return (
-    <div className="max-w-3xl mx-auto p-4">
-        <nav className="flex py-4 my-4 items-center justify-between">
-            <h1 className="text-2xl font-bold"><FontAwesomeIcon icon={faCode}></FontAwesomeIcon> jemcv</h1>
-            <ul className="flex text-xl items-center gap-2 font-medium">
+    <div className="max-w-4xl mx-auto p-4">
+        <nav className="flex items-center justify-between py-4 my-4">
+            <h1 className="font-black"><FontAwesomeIcon icon={faCode}></FontAwesomeIcon> JEMCV</h1>
+            <ul className="flex gap-2 items-center font-bold">
                 <li>about</li>
-                <li className="bg-slate-700 text-slate-100 py-2 px-2 rounded cursor-pointer"><a href="" download>resume</a></li>
+                <li className="bg-green-500 text-slate-100 p-2 rounded cursor-pointer"><a href="" download>📝resume</a></li>
             </ul>
         </nav>
-        <header className="flex justify-center my-4 py-4 leading-8 flex-nowrap">
-                <img className="w-48 border-8 border-slate-700" src={me} alt="a picture"/>
-                <p className='ml-8'>
-                    <h1 className="text-2xl font-bold">Hello there! Im Jem </h1>
-                    <p className="mt-2">A Muggle-Born Wizard from Hogwarts.</p>
-                    <p>Passionate about building websites and mobile apps.</p>
-                    <p>Linux Enthusiast and occasional ukulele strummer.</p>
-                    <section className="mt-6 ">
-                        <a className=" font-bold text-lg p-1 cursor-pointer" href="https://github.com" target='blank'><FontAwesomeIcon icon={ faGithub}></FontAwesomeIcon> Github</a>
-                    </section>
-                </p>
+        <header className="grid gap-4 my-4 md:grid-cols-2 md:items-center md:justify-center">
+                <img dir="ltr" className="w-48 mx-auto rounded-full border-4 border-y-orange-400 border-x-neutral-900" src={me} alt="a picture"/>
+                <div className="">
+                        <h1 className="font-bold">Hello there!👋  Im Jem</h1>
+                        <p>A Muggle-Born Wizard ⚡️ from Hogwarts.</p>
+                        <p>Passionate ❤️ about building websites and mobile apps.</p>
+                        <p>Occasional ukulele strummer 🎵.</p>
+                        <section className="mt-4 flex flex-col gap-2 md:flex-row md:gap-4">
+                            <a href="https://github.com" target='blank'><FontAwesomeIcon icon={ faGithub}></FontAwesomeIcon> Github</a>
+                            <a className="text-sky-800"href="https://linkedin.com" target='blank'><FontAwesomeIcon icon={ faLinkedin}></FontAwesomeIcon> Linkedin</a>
+                        </section>
+                </div>
         </header>
+        <hr />
+        
  
    </div>
   )
