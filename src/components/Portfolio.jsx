0ac628371f31caefb23c60afdd1437de0256b2e5
jemcv <rangothechameleon@gmail.com>
2023-10-10 16:59:42 +0800

    Update icons

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,7 +14,7 @@ function Portfolio() {
         <nav className="flex items-center justify-between md:py-2 md:my-2">
             <h1 className="font-bold text-lg "><FontAwesomeIcon icon={faCode}></FontAwesomeIcon> JEMCV</h1>
             <ul className="flex gap-2 items-center">
-                <li className="bg-neutral-900 text-slate-50 p-2 rounded cursor-pointer font-medium"><a href="" download>📝Resume</a></li>
+                <li className="bg-neutral-900 text-slate-50 p-2 rounded cursor-pointer font-medium"><a href="" download>📝 Resume</a></li>
             </ul>
         </nav>
         <header className="grid gap-4 my-4 md:grid-cols-2 md:items-center md:justify-center">
@@ -23,8 +23,8 @@ function Portfolio() {
                         <h1 className="font-extrabold text-base text-center mb-2 md:text-left md:text-lg">Hey!👋 Im Jem.</h1>
                         <p className="text-sm font-medium text-center md:text-left md:text-base">A Muggle-Born Wizard ⚡️ from Hogwarts. <br/>Passionate ❤️ about web and mobile app development. <br/>Occasional ukulele strummer 🎵.</p>
                         <section className="mt-4 flex flex-col gap-2 md:flex-row md:gap-4">
-                            <a className="text-slate-900 font-semibold" href="https://github.com/jemcv" target='_blank'><FontAwesomeIcon icon={ faGithub}></FontAwesomeIcon> Github</a>
-                            <a className="text-sky-800 font-semibold" href="https://linkedin.com" target='_blank'><FontAwesomeIcon icon={ faLinkedin}></FontAwesomeIcon> Linkedin</a>
+                            <a className="text-slate-900 font-semibold text-lg" href="https://github.com/jemcv" target='_blank'><FontAwesomeIcon icon={ faGithub}></FontAwesomeIcon> Github</a>
+                            <a className="text-sky-800 font-semibold text-lg" href="https://linkedin.com" target='_blank'><FontAwesomeIcon icon={ faLinkedin}></FontAwesomeIcon> Linkedin</a>
                         </section>
                 </div>
         </header>
@@ -32,14 +32,10 @@ function Portfolio() {
         <section>
             <h1 className="font-bold md:text-lg">SKILLS 👷🏽</h1>
                 <ul className="flex flex-wrap justify-center items-center m-2 md:flex-nowrap">
-                    <li className="p-2 rounded-md text-orange-500 text-3xl md:text-4xl"><FontAwesomeIcon icon={ faHtml5}></FontAwesomeIcon></li>
-                    <li className="p-2 rounded-md text-blue-500 text-3xl md:text-4xl" ><FontAwesomeIcon icon={ faCss3}></FontAwesomeIcon></li>
-                    <li className="p-2 rounded-md text-yellow-400 text-3xl md:text-4xl"><FontAwesomeIcon icon={ faJs}></FontAwesomeIcon></li>
-                    <li className="p-2 rounded-md text-blue-900 text-3xl md:text-4xl"><FontAwesomeIcon icon={ faPhp}></FontAwesomeIcon></li>
-                    <li className="p-2 rounded-md text-blue-500 text-3xl md:text-4xl" ><FontAwesomeIcon icon={ faPython}></FontAwesomeIcon></li>
-                    <li className="p-2 rounded-md text-blue-400 text-3xl md:text-4xl" ><FontAwesomeIcon icon={ faReact}></FontAwesomeIcon></li>
-                    <li className="p-2 rounded-md  text-blue-400 text-3xl md:text-4xl" ><BiLogoTailwindCss icon={ BiLogoTailwindCss }></BiLogoTailwindCss></li>
-                </ul>
+          <a className="p-2" href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=html,css,js,php,mysql,figma,react,tailwindcss,git,linux,neovim" />
+   </a> 
+                                    </ul>
         </section>
         <hr />
         
@@ -49,23 +45,25 @@ function Portfolio() {
                 <div className="bg-gray-200 p-4 rounded-md">
                     <h1 className="mb-2 font-semibold">Blog Website ✏️</h1>
                     <p className="text-sm font-medium">Built with Zola to post my codes, write blogs, and share Linux-related content.</p>
-                    <a href="https://jemcv.github.io/web" target="_blank"><img src={zola} alt="Visit the Zola site" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md border-4 border-gray-900"/></a>
+                    <a href="https://jemcv.github.io/web" target="_blank"><img src={zola} alt="Visit the Zola site" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md "/></a>
                 </div>
                 <div className="bg-gray-200 p-4 rounded-md">
                     <h1 className="mb-2 font-semibold">Learning Management Sytem 📌</h1>
                     <p className="text-sm font-medium">This one was developed utilizing PHP as the backend and SQL as the database. </p>
-                    <img src={lms} alt="random image" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md border-4 border-gray-900"/>
+                    <img src={lms} alt="random image" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md "/>
                 </div>
+          {/*
                 <div className="bg-gray-200 p-4 rounded-md">
                     <h1 className="mb-2 font-semibold">Portfolio Website 🎨</h1>
                     <p className="text-sm font-medium">Developed this project using ReactJS, Tailwind CSS, and Vite.</p>
-                    <img src={pfl} alt="random image" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md border-4 border-gray-900"/>
+                    <img src={pfl} alt="random image" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md "/>
                 </div>
                 <div className="bg-gray-200 p-4 rounded-md">
-                    <h1 className="mb-2 font-semibold">Mobile Application ⬆️</h1>
+                    <h1 className="mb-2 font-semibold">Mobile Application 📱</h1>
                     <p className="text-sm font-medium">A simple endless-scroll mobile app using Dart and Flutter.</p>
-                    <img src={wla} alt="random image" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md border-4 border-gray-900"/>
+                    <img src={wla} alt="random image" className="mt-2 mx-auto w-auto h-64 object-cover rounded-md "/>
                 </div>
+          */}
             </section>
         </section>
        
