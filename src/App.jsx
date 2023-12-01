361e7c0b6b981fc823a70c3b8b0136b55b3b02d9
jemcv <rangothechameleon@gmail.com>
2023-12-02 00:27:38 +0800

    Update bg

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Main from './components/Portfolio'
 function App() {
 
   return (
-    <div className="bg-neutral-950 text-gray-200">
+    <>
       <Main />    
-    </div>
+    </>
   )
 }
 
